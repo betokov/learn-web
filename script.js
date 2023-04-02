7eaa4a166ff70fa4f689323347ab3dc025ae978e
Barasbi Betokov <web_masters_07@mail.ru>
2023-04-02 20:18:15 +0300

    added stage 2

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let button = document.querySelector(".light");
-  let checkbox = document.querySelector(".light .checkbox");
-  let themeName = document.querySelector(".light span");
+  let defaultChecked = sessionStorage.getItem("checked");
+  let button = document.querySelector(".light")
+  let checkbox = document.querySelector(".light .checkbox")
+  let themeName = document.querySelector(".light span")
 
-  if(document.body.classList.contains('dark')) {
-    themeName.textContent = "Темная";
-    checkbox.checked = true;
+  if(defaultChecked !== null) {
+    checkbox.checked = true
+    document.body.classList.add("dark")
+    themeName.textContent = "Темная"
+  }
+
+  function checkCheckbox(checkbox) {
+    if(checkbox.checked) {
+      document.body.classList.add("dark")
+      themeName.textContent = "Темная"
+      sessionStorage.setItem("checked", "true")
+    } else {
+      document.body.classList.remove("dark")
+      themeName.textContent = "Светлая"
+      sessionStorage.removeItem("checked")
+    }
   }
 
   button.addEventListener("click", function() {
-    if(checkbox.checked) {
-      document.body.classList.add("dark");
-      themeName.textContent = "Темная";
-    } else {
-      document.body.classList.remove("dark");
-      themeName.textContent = "Светлая";
-    }
-  });
+    checkCheckbox(checkbox)
+  })
+
+  checkCheckbox(checkbox)
 });
